32212221d4a5820e59a49501ef6772b6d959a046
grant_uhn <guan.chen@uhn.ca>
2023-04-17 14:29:08 -0400

    refactor

--- codec.py
+++ codec.py
@@ -2,6 +2,7 @@ def get_encrypted_password(password):
   # use ENCRYPTION_KEY to encrypt a password
   pass
 
+
 def get_decrypted_password(encrypted_passed):
   # use ENCRYPTION_KEY to decrypt a password
   pass
