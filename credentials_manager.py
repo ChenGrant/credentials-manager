565cfd24f34496bf6c3773d4f781349394ad029c
Productivity <grantchen2021@gmail.com>
2023-04-18 12:55:30 -0400

    made changes

--- credentials_manager.py
+++ credentials_manager.py
@@ -2,33 +2,17 @@ import database_manager as db
 
 
 def main():
-  def render_commands(commands):
-    print()
-    print("------ Commands ------")
-    for index, command in enumerate(commands):
-      print(f"[{index}] - {command}")
-      
-  # https://stackoverflow.com/questions/10865483/print-results-in-mysql-format-with-python
   def render_credentials(credentials):
-    if credentials == []:
+    if not credentials:
       print("No credentials")
       return
-      
-    column_widths = {key: len(key) for key in credentials[0].keys()}
+
+    col_widths = {key: max(max([len(entry[key]) for entry in credentials]), len(key)) 
+                  for key in credentials[0].keys()}
+    print("|".join([name + (width - len(name)) * ' ' for name, width in col_widths.items()]))
+    print("-"* (sum(map(lambda x : x + 1, col_widths.values())) - 1))
     for entry in credentials:
-      for key, val in entry.items():
-        column_widths[key] = max(len(val), column_widths[key])
-    table_width = sum(map(lambda x : x + 1, column_widths.values())) + 1
-    print("-" * table_width)
-    for column_name, column_length in column_widths.items():
-      print(f"|{column_name}{(column_length - len(column_name))*' '}", end="")
-    print("|")
-    print("-" * table_width)
-    for entry in credentials:
-      for key, val in entry.items():
-        print(f"|{val}{(column_widths[key] - len(val))*' '}", end="")
-      print("|")
-    print("-" * table_width)
+      print("|".join([val + (col_widths[key] - len(val)) * ' ' for key, val in entry.items()]))
     
     
   user_id = 1 #None
@@ -53,65 +37,61 @@ def main():
       
     if not user_id:
       print("Try Again")
-  
-  # make adding command easier, map command name to number 
-  COMMANDS = [
-    "get all credentials",
-    "get credentials connected to an app",
-    "get credentials connected to an email",
-    "add app credentials",
-    "update app credentials",
-    "delete app credentials",
-    "get user credentials",
-    "update user credentials",
-    "quit"
-  ]
+
   
   while True:
-    render_commands(COMMANDS)
-    command_index = input("Enter a command: ")
+    print("\n------ Commands ------")
+    for command, name in {
+      "gc": "get all credentials",
+      "gca": "get credentials connected to an app",
+      "gce": "get credentials connected to an email",
+      "aac": "add app credentials",
+      "uac": "update app credentials",
+      "dac": "delete app credentials",
+      "guc": "get user credentials",
+      "uuc": "update user credentials",
+      "q": "quit"
+    }.items():
+      print(f"[{command}] - {name}")
+      
+    command = input("Enter a command: ")
     
-    try:
-      command = COMMANDS[int(command_index)]
-    except:
-      command = None
-    
-    if command == "get all credentials" :
+    if command == "gc" :
       credentials = db.get_all_credentials(user_id)
       if credentials is not None: 
         render_credentials(credentials)
       continue
     
-    if command == "get credentials connected to an app" :
+    if command == "gca" :
       app = input("Enter an app: ")
       credentials = db.get_credentials_from_app(user_id, app)
       if credentials is not None: 
         render_credentials(credentials)
       continue
     
-    if command == "get credentials connected to an email" :
+    if command == "gce" :
       email = input("Enter an email: ")
       credentials = db.get_credentials_from_email(user_id, email)
       if credentials is not None: 
         render_credentials(credentials)
       continue
     
-    if command == "add app credentials" :
+    if command == "aac" :
       continue
     
-    if command == "update app credentials" :
+    if command == "uac" :
       continue
     
-    if command == "delete app credentials":
+    if command == "dac":
       continue
     
-    if command == "get user credentials":
+    if command == "guc":
       continue
     
-    if command == "update user credentials":
+    if command == "uuc":
       continue
     
-    if command == "quit":
+    if command == "q":
       continue
     
     print("Try Again")
