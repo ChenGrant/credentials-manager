c68d89cb453eeeed79094b3da0ba8b2d3ec8777e
Productivity <grantchen2021@gmail.com>
2023-04-18 01:27:57 -0400

    finished user_getting

--- credentials_manager.py
+++ credentials_manager.py
@@ -3,24 +3,62 @@ import database_manager as db
 def main():
   user_id = None
   
-  while True:
-    user_command = input("signup [s] or login [l]: ")
+  while not user_id:
+    print()
+    print("------ Commands ------")
+    print("[s] - signup")
+    print("[l] - login")
     
-    if user_command == "s":
-      username = input("enter username: ")
-      password = input("enter password: ")
+    command = input("Enter a command: ")
+    
+    if command == "s" :
+      username = input("Enter username: ")
+      password = input("Enter password: ")
       user_id = db.signup_user(username, password)
-    
-    if user_command == "l":
-      username = input("enter username: ")
-      password = input("enter password: ")
+      
+    if command == "l":
+      username = input("Enter username: ")
+      password = input("Enter password: ")
       user_id = db.login_user(username, password)
-    
-    if (user_id): break
-    
-    print("Try Again.")
+      
+    if not user_id:
+      print("Try Again")
   
-  print(f"user authenticated. user_id: {user_id}")
+  
+  while True:
+    print()
+    print("------ Commands ------")
+    print("[1] - get credentials for an app")
+    print("[2] - get all apps connected to an email")
+    print("[3] - add app credentials")
+    print("[4] - update app credentials")
+    print("[5] - delete app credentials")
+    print("[6] - get user credentials")
+    print("[7] - update user credentials")
+    print("[q] - quit")
+    
+    command = input("Enter a command: ")
+    
+    if command == "1" :
+      continue
+    
+    if command == "2" :
+      continue
+    
+    if command == "3" :
+      continue
+    
+    if command == "4" :
+      continue
+    
+    if command == "5" :
+      continue
+    
+    if command == "q":
+      continue
+    
+    print("Try Again")
+    
 
 if __name__ == "__main__":
   main()
--- database_manager.py
+++ database_manager.py
@@ -1,8 +1,31 @@
 from codec import encrypt, decrypt
 import config
 import mysql.connector
+from mysql.connector import errorcode
 import os
 
+# establish mysql connection
+connection = mysql.connector.connect(
+  host=os.environ.get("HOST"), 
+  password=os.environ.get("PASSWORD"), 
+  user=os.environ.get("USER"),
+  database=os.environ.get("DATABASE"))
+
+cursor = connection.cursor()
+
+
+# functions
+def try_catch(func):
+  def wrapper(*args, **kwargs):
+    try: 
+      return func(*args, **kwargs)
+    except BaseException as e: 
+      print("Error: ", e)
+
+  return wrapper
+      
+      
+@try_catch
 def signup_user(username, password):
   cursor.execute(
     f"INSERT INTO users (username, password)\n"
@@ -11,41 +34,38 @@ def signup_user(username, password):
   connection.commit()
   cursor.execute(
     f"SELECT id FROM users\n"
-    f"WHERE username = '{username}'"
+    f"WHERE username = '{username}'\n"
     f"LIMIT 1"
   )
   return next(cursor)[0]
-  
-  
+    
+
+@try_catch
 def login_user(username, password):
   cursor.execute(
-    f"SELECT id, password FROM users\n"
-    f"WHERE username = '{username}'"
+    f"SELECT id FROM users\n"
+    f"WHERE username = '{username}'\n"
+    f"AND password = '{decrypt(password)}'\n"
     f"LIMIT 1"
   )
-  user = next(cursor)
-  return user[0] if decrypt(user[1]) == password else None
-
-
-def get_apps(user_id, email):
-  pass
+  return next(cursor)[0]
 
 
 def get_credentials(user_id, app):
   pass
 
 
-def add_credential(user_id, app, email, username, password):
+def get_apps(user_id, email):
   pass
 
 
-def update_credential(user_id, app, email, username, password):
+def add_credentials(user_id, app, email, username, password):
   pass
 
-connection = mysql.connector.connect(
-  host=os.environ.get("HOST"), 
-  password=os.environ.get("PASSWORD"), 
-  user=os.environ.get("USER"),
-  database=os.environ.get("DATABASE"))
 
-cursor = connection.cursor()
+def update_credentials(user_id, app, email, username, password):
+  pass
+
+
+def delete_credentials(user_id, app):
+  pass
